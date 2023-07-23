5ac124c2a9e5daac23cd06814c69fb0dd55a8807
jay2010 <jay.rgukt@outlook.com>
2023-07-23 20:45:21 +0530

    basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WorkspaceComponent } from './workspace/workspace.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { LeftPanelComponent } from './left-panel/left-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkspaceComponent,
+    SearchBarComponent,
+    LeftPanelComponent
   ],
   imports: [
     BrowserModule
