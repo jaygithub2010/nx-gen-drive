8229d3ff7a953c8a4d20cfc9c45feade3565ead5
Vijay <jay.rgukt@outlook.com>
2025-06-18 23:49:21 +0530

    updating angular

--- src/app/explorer/advance-search/advance-search.component.ts
+++ src/app/explorer/advance-search/advance-search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchOptions } from 'src/app/models/search-options';
-import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+// import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-advance-search',
