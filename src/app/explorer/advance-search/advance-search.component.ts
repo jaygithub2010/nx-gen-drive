c69a92a04dd06d0b36d35c845efa2ea7a3e6f406
Vijay <jay.rgukt@outlook.com>
2025-06-20 00:37:49 +0530

    after update to angular 19

--- src/app/explorer/advance-search/advance-search.component.ts
+++ src/app/explorer/advance-search/advance-search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchOptions } from 'src/app/models/search-options';
-// import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-advance-search',
@@ -10,6 +10,8 @@ import { SearchOptions } from 'src/app/models/search-options';
   styleUrl: './advance-search.component.css'
 })
 export class AdvanceSearchComponent implements OnInit{
+    // constructor(public activeModal: NgbActiveModal) {} 
+
   searchOptions:SearchOptions;
   ngOnInit(): void {
     
--- src/app/explorer/search-bar/search-bar.component.ts
+++ src/app/explorer/search-bar/search-bar.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AdvanceSearchComponent } from "../advance-search/advance-search.component";
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-search-bar',
@@ -10,5 +11,10 @@ import { AdvanceSearchComponent } from "../advance-search/advance-search.compone
   imports: [CommonModule, AdvanceSearchComponent]
 })
 export class SearchBarComponent {
+  constructor(private ngbModalService: NgbModal) {}
 
+    openAdvanceSearch() {
+    const modalRef = this.ngbModalService.open(AdvanceSearchComponent);
+    // modalRef.componentInstance.searchOptions = this.searchOptions;
+}
 }
